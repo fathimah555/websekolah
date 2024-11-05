ea31699a433c8b65ccf41d52be0a1063af74cc7c
fathimah555 <fathimahmeh@gmail.com>
2024-11-05 10:52:12 +0700

    Update New 

--- app/Models/jurusan.php
+++ app/Models/jurusan.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Models\Jurusan;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class jurusan extends Model
+class Jurusan extends Model
 {
     protected $table = 'jurusan';
 
